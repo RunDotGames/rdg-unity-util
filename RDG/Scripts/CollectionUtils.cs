aceee7b6dd73cc868524ed5236852eb5403b163e
Mark Mayer <mdm373@gmail.com>
2021-09-06 14:28:04 -0400

    initial utils

---
+++ RDG/Scripts/CollectionUtils.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace RDG.UnityUtil {
+  public static class CollectionUtils {
+    
+    public static IEnumerable<T> Once<T>(T value) {
+      yield return value;
+    }
+  }
+}
---
+++ RDG/Scripts/CoroutineUtils.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+namespace RD.UnityUtil {
+  
+  public static class CoroutineUtils {
+    public static readonly YieldInstruction EndOfFrame = new WaitForEndOfFrame();    
+  }
+}
